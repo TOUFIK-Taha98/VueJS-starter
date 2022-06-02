70431ad6d725a59dd13ae5dda5808403d2c24fcc
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 14:39:17 +0200

    V-if v-else se connceter/se deconnecter

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>    
+  <div>   
     <Exo1/>
     <Exo2/>
     <Exo3/>
     <Exo4/>
-    <Exo5/>
+    <DemoVFor/>
+    <Conditionels/>
   </div>
 </template>
 <script setup>
@@ -13,7 +14,8 @@
   import Exo2 from '@/components/exos/Exo2.vue';
   import Exo3 from '@/components/exos/Exo3.vue';
   import Exo4 from '@/components/exos/Exo4.vue';
-  import Exo5 from '@/components/exos/Exo5.vue';
+  import DemoVFor from '@/components/exos/DemoVFor.vue';
+  import Conditionels from '@/components/exos/Conditionels.vue';
 
 </script>
 
---
+++ src/components/exos/Conditionels.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { ref } from 'vue'
+    const user = ref(null)
+
+</script>
+
+<template>
+    <h1>Hello, les tests v-if</h1>
+    <hr/>
+    <h2>Hello {{user ? user : 'Anonyme'}}</h2>
+    <button v-if="!user">Se connecter</button>
+    <button v-if="user">Se déconnecter</button>
+</template>
