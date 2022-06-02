9fb3488c9d7543d380f89bbdca936f5ab3de4936
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 11:11:58 +0200

    Exo 3 !

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
+    <h1>You did it !</h1>
     <h2>Exo 2</h2>
     <p>{{ counter }}</p>
     <button @click="submit">+</button>
     <button @click="reset">RESET</button>
     <br />
-    
+    <hr>
+    <h2>Exo 3</h2>
     <input type="text" v-model="token"/>
-    <br/> <button @click="getToken">TEST - Token</button>
+    <br/> 
     {{ token }}
+    <button @click="decodeClaimns">TEST - Token</button>
+    {{ claims }}
   </div>
 </template>
 <script setup>
@@ -17,6 +21,8 @@
 
   const counter = ref(0);
   const token = ref('');
+  const claims = ref(null);
+
   const submit = () => {
     counter.value ++;
   }
@@ -24,8 +30,9 @@
     counter.value = 0;
   }
 
-  const getToken = () => {
-    const claims = jose.decodeJwt(token)
+  const decodeClaimns = () => {
+    const jwtdecode = jose.decodeJwt(token.value)
+    claims.value = jwtdecode;
     console.log(claims)
   }
 
