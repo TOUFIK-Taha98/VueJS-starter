6568839ba0e9b06c4d03b3e41dbeac70defa02b8
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 10:30:24 +0200

    Exo 2 !

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
+<template>
+  <div>
+    <h2>Exo 2</h2>
+    <p>{{ this.counter }}</p>
+    <input type="text" v-model="counter" />
+    <button @click="submit">+</button>
+    <button @click="reset">RESET</button>
+  </div>
+</template>
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  name: "Test",
   data() {
     return {
-      count: 0,
+      counter: 0,
     };
+  },
+  methods: {
+    submit() {
+      this.counter++;
+      console.log(this.ref);
+    },
+    reset() {
+      this.counter = 0;
+    },
   },
 };
 </script>
-
-<template>
-  {{ count }}
-</template>
