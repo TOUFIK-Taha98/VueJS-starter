73f38f08022c6c0ad0b21bd6533f6e919b579895
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 12:22:26 +0200

    avoir une formulaire (input v-model, button @click, + une fonction) qui va ajouter un élément au tableau; {id, nom, prenom} _ bonus si vous cliquez sur un élément affichén ca le supprime

--- src/components/exos/Exo5.vue
+++ src/components/exos/Exo5.vue
@@ -1,22 +1,29 @@
 <script setup>
     import { ref } from 'vue';
+    const newUser = ref('');
     const tableau = ref([1, 2, 3]);
     const listeObjets = ref([
         {id: 0, name:'Alice'},
         {id: 1, name:'Bob'},
         {id: 2, name:'Charlie'}
     ])
+
+    const saveUser = () =>{
+        listeObjets.value.push({id:10, name: newUser.value})
+        newUser.value="";
+    }
+    const deleteUser = (user) => {
+        console.log(user.id)
+        listeObjets.value = listeObjets.value.filter(u => u.id != user.id)
+    }
 </script>
 
 <template>
     <h2>Exo5 : V-FOR v-IF v-ELSE</h2>
-    <p v-for="i in tableau">{{i}}</p>
     <ul>
-        <li v-for="obj in listeObjets" :key="obj.id">{{obj.name}}</li>
+        <li v-for="obj in listeObjets" :key="obj.id" @click="()=> deleteUser(obj)">{{obj.name}}</li>
     </ul>
-
-    <div v-for="obj in listeObjets" :key="obj.id">
-        <h2>{{ obj.name }}</h2>
-        <p>{{ obj.id }}</p>
-    </div>
+    <input type="text" v-model="newUser"/>
+    <button @click="saveUser">Save User</button>
+    <hr/>
 </template>
