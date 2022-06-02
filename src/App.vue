307fa97c49457bbfe198cc9207b9c54c04c1a028
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 12:09:01 +0200

    V-FOR

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <Exo2/>
     <Exo3/>
     <Exo4/>
+    <Exo5/>
   </div>
 </template>
 <script setup>
@@ -12,6 +13,7 @@
   import Exo2 from '@/components/exos/Exo2.vue';
   import Exo3 from '@/components/exos/Exo3.vue';
   import Exo4 from '@/components/exos/Exo4.vue';
+  import Exo5 from '@/components/exos/Exo5.vue';
 
 </script>
 
---
+++ src/components/exos/Exo5.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { ref } from 'vue';
+    const tableau = ref([1, 2, 3]);
+    const listeObjets = ref([
+        {id: 0, name:'Alice'},
+        {id: 1, name:'Bob'},
+        {id: 2, name:'Charlie'}
+    ])
+</script>
+
+<template>
+    <h2>Exo5 : V-FOR v-IF v-ELSE</h2>
+    <p v-for="i in tableau">{{i}}</p>
+    <ul>
+        <li v-for="obj in listeObjets" :key="obj.id">{{obj.name}}</li>
+    </ul>
+
+    <div v-for="obj in listeObjets" :key="obj.id">
+        <h2>{{ obj.name }}</h2>
+        <p>{{ obj.id }}</p>
+    </div>
+</template>
