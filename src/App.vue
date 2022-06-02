c9946713919817f6017919c5d7dcdeae4060f32c
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 10:21:21 +0200

    First commit after installations

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  {{ count }}
+</template>
---
+++ src/components/__tests__/HelloWorld.spec.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    expect(true).toBe(true)
+  })
+})
