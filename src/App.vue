7f12f6fdbbe9dfc744ea2bd993fe4a1bcaf5f740
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 10:59:06 +0200

    Using jose to decode a token

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,27 @@
     <button @click="reset">RESET</button>
     <br />
     
+    <input type="text" v-model="token"/>
+    <br/> <button @click="getToken">TEST - Token</button>
+    {{ token }}
   </div>
 </template>
 <script setup>
   import { ref } from 'vue';
-  const counter = ref(0);
+  import * as jose from 'jose'
 
+  const counter = ref(0);
+  const token = ref('');
   const submit = () => {
     counter.value ++;
   }
   const reset = () => {
     counter.value = 0;
+  }
+
+  const getToken = () => {
+    const claims = jose.decodeJwt(token)
+    console.log(claims)
   }
 
 </script>
