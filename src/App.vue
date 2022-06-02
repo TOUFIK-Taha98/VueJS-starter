fe1cbc12ddbd338ab115201774b711c5c02c8785
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 16:00:25 +0200

    Implemente our first route '/' and display a component on this route

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
-<template>
-  <div>   
-    <!--<Exo1/>
-    <Exo2/>
-    <Exo3/>
-    <Exo4/>
-    <DemoVFor/> -->
-    <Conditionels/>
-  </div>
-</template>
 <script setup>
 
-  import Exo1 from '@/components/exos/Exo1.vue';
-  import Exo2 from '@/components/exos/Exo2.vue';
-  import Exo3 from '@/components/exos/Exo3.vue';
-  import Exo4 from '@/components/exos/Exo4.vue';
-  import DemoVFor from '@/components/exos/DemoVFor.vue';
-  import Conditionels from '@/components/exos/Conditionels.vue';
-
 </script>
+
+<template>
+  <RouterView/>
+</template>
 
 <style>
   @import 'bootstrap/dist/css/bootstrap.min.css';
--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
 import { createWebHistory, createRouter } from "vue-router";
+import Exo2 from '@/components/exos/Exo2.vue'
 
 const router = createRouter({
-    history: createWebHistory,
+    history: createWebHistory(),
     routes: [
-
+        {path: '/', name:'exo2', component:Exo2}
     ]
 })
 
